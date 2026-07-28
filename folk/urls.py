1c12f688f78dd70658f20246366ee0026eab6194
Kashan <kashanali.mastuj@gmail.com>
2026-07-28 21:34:31 +0500

    about section

--- folk/urls.py
+++ folk/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from . import views
 urlpatterns=[
     path('',views.home, name="home"),
-    path('/about',views.about,name="about"),
-    path('/upload',views.upload,name="upload"),
-    path('/login',views.login,name="login")
+    path('about/',views.about,name="about"),
+    path('upload/',views.upload,name="upload"),
+    path('login/',views.login,name="login")
 ]
