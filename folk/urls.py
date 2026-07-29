872e7d9689611fcf91196d380b764c92c94144ad
Kashan <kashanali.mastuj@gmail.com>
2026-07-29 15:41:06 +0500

    finished login page

--- folk/urls.py
+++ folk/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 urlpatterns=[
     path('',views.home, name="home"),
     path('about/',views.about,name="about"),
     path('upload/',views.upload,name="upload"),
-    path('login/',views.login,name="login")
+    path('login/', auth_views.LoginView.as_view(template_name='folk/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
