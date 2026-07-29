53c45a7633d76cfbc6373734756d0bf5c37d1600
Kashan <kashanali.mastuj@gmail.com>
2026-07-29 12:01:26 +0500

    Made a stories database

--- folk/models.py
+++ folk/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Story(models.Model):
+    title = models.CharField(max_length = 255, help_text = "The name of the tale,myth, or legend")
+    content = models.TextField(help_text = "The full text of the Story here")
+    narrator = models.CharField(max_length = 255 , help_text = "The person who told, or remembered the story")
+    
+
+    audio_recording = models.FileField(
+        upload_to = 'audio/stories',
+        null = True,
+        blank = True,
+        help_text = 'Optional voice recording of the story'
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name_plural = "Stories"
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.title} (Told by: {self.narrator})"
--- folk/views.py
+++ folk/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
+from .models import Story
 
 # Create your views here.
 
-def home(request):
-    return render(request, "folk/home.html")
 def about(request):
     return render(request, "folk/about.html")
+
 def upload(request):
     return render(request,"folk/upload.html")
+
 def login(request):
     pass
+
+def home(request):
+    stories = Story.objects.all()
+    context = {
+        'stories' : stories
+    }
+    return render(request, 'folk/home.html',context)
+
