8937ca1db256f3cdcfa1051cd9e93378ede113dc
Kashan <kashanali.mastuj@gmail.com>
2026-08-07 21:51:04 +0500

    uptime robot ready

--- Ishlogh/urls.py
+++ Ishlogh/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings            
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+# A tiny view that just returns text
+def ping_view(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('folk/',include('folk.urls'))
+    path('folk/',include('folk.urls')),
+    path('ping/', ping_view),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
